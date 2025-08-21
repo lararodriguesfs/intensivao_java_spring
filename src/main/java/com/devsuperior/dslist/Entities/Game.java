71b7deca8fd838739435d8b2f06f0cf17c8c7776
Lara Rodrigues <lararfonseca16@gmail.com>
2025-08-21 13:58:09 -0300

    Game seed

--- src/main/java/com/devsuperior/dslist/Entities/Game.java
+++ src/main/java/com/devsuperior/dslist/Entities/Game.java
@@ -22,6 +22,7 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+    private Double score;
     private String imgUrl;
 
     @Column(columnDefinition = "TEXT")
@@ -33,13 +34,14 @@ public class Game {
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platforms, String imgUrl,
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
                 String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -85,6 +87,13 @@ public class Game {
         this.platforms = platforms;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
 
     public String getImgUrl() {
         return imgUrl;
